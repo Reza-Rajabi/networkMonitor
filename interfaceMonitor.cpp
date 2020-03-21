e3c6c8dfed26c0af85cb8e90dac4cfd0fbfba61d
rare <reza.rjbi@yahoo.com>
2020-03-21 01:52:20 -0400

    Almost done interfaceMonitor

--- interfaceMonitor.cpp
+++ interfaceMonitor.cpp
@@ -13,147 +13,135 @@
 #include <signal.h>
 #include <sys/socket.h>
 #include <sys/un.h>
-#include <vector>
+#include <sys/ioctl.h>
+#include <net/if.h>
 #include <string>
-
-typedef void (*sighandler_t)(int);
-enum exit_err_code { ERR_SIG = 1, ERR_SOCK, ERR_CONNECT, ERR_OPEN_DIR,
-                     ERR_FORK, IGNORE };
-
-const char* statsTitle[] { "tx_bytes", "tx_dropped", "tx_errors", "tx_packets",
-                           "rx_bytes", "rx_dropped", "rx_errors", "rx_packets" };
-enum stats_index         { tx_bytes, tx_dropped, tx_errors, tx_packets,
-                           rx_bytes, rx_dropped, rx_errors, rx_packets };
-// MISSING operstate, carrier_up_count, carrier_down_count
 
 
 const int MAX_BUF = 255;
-const int NUM_STAT = 8; /// number of words in statsTitle --------------> NEEDS EDITING WHEN missing RESOLVED
-const char netPath[] = "/sys/class/net";
-const char statPath[] = "/sys/class/net/%s/statistics/";
-char socketPath[MAX_BUF] = "/tmp/interfaceMonitor/";
-char path[MAX_BUF], temp[MAX_BUF];
+const int MIN_BUF = 50;
+const int NUM_STAT = 11;
+char netPath[MIN_BUF] = "/sys/class/net/"; /// interface name will be added here
+char socketPath[MIN_BUF] = "/tmp/interfaceMonitor/";
+char path[MIN_BUF], box[MIN_BUF];
 bool isRunning = true;
 int sockFd;
 
+enum exit_err_code { ERR_ARG = 1, ERR_SIG, ERR_SOCK, ERR_CONNECT,
+                     ERR_OPEN_DIR, ERR_FORK, ALERT };
 
-void handleError(std::string msg1, char* msg2, exit_err_code err = IGNORE, int fd = -1);
-void loadStatistics(char* interfaceName, int* stats);
+enum state { UP = 1, DOWN = 0, UNKNOWN = -1 };
 
+struct stat {
+    char subPath[MIN_BUF];
+    char title[MIN_BUF];
+    int val;
+};
+
+stat STAT[] = {
+    {"/operstate"         , "state"     , 0}, /// will map an int to `up` , `down` , and `unknown` state
+    {"/carrier_up_count"  , "up_count"  , 0},
+    {"/carrier_down_count", "down_count", 0},
+    {"/statistics/"       , "tx_bytes"  , 0},
+    {"/statistics/"       , "tx_packets", 0},
+    {"/statistics/"       , "tx_dropped", 0},
+    {"/statistics/"       , "tx_errors" , 0},
+    {"/statistics/"       , "rx_bytes"  , 0},
+    {"/statistics/"       , "rx_packets", 0},
+    {"/statistics/"       , "rx_dropped", 0},
+    {"/statistics/"       , "rx_errors" , 0},
+};
+
+
+typedef void (*sighandler_t)(int);
 static void signalHandler(int signal);
+void handleError(std::string msg1, char* msg2, exit_err_code err = ALERT, int fd = -1);
+void talk(const char* toSay, char* answer);
+void loadStatistics(char* interfaceName, int* stats);
+void monitor(struct dirent dir, pid_t& pid);
 
+
+
+//------------------------------------- MAIN ------------------------------------------//
 int main(int argc, char *argv[]) {
+    // check the path to the requested interface
+    if (argc != 2) handleError("Interface name not provided", NULL, ERR_ARG);
+    strcat(netPath, argv[1]);
+    
     // register signal handler
     sighandler_t err = signal(SIGINT, signalHandler);
-    if (err == SIG_ERR)
-        handleError("could not creat a signal handler", nullptr, ERR_SIG);
+    if (err == SIG_ERR) handleError("could not creat a signal handler", NULL, ERR_SIG);
     
     // setup socket communication
     sockFd = socket(AF_UNIX, SOCK_STREAM, 0);
-    if (sockFd == -1)
-        handleError("error on creating socket", nullptr, ERR_SOCK);
+    if (sockFd == -1) handleError("error on creating socket", NULL, ERR_SOCK);
     struct sockaddr_un sockAddr;
     bzero(&sockAddr, sizeof(sockAddr));
     sockAddr.sun_family = AF_UNIX;
-    bzero(temp, MAX_BUF);
-    sprintf(temp,"%d", getpid());
-    strcat(socketPath,temp);
+    bzero(box, MAX_BUF);
+    sprintf(box,"%d", getpid());
+    strcat(socketPath,box);
     strncpy(sockAddr.sun_path, socketPath, sizeof(sockAddr.sun_path) - 1);
     
     // connect to local socket
     int status = connect(sockFd, (struct sockaddr*) &sockAddr, sizeof(sockAddr));
-    if (status == -1)
-        handleError("error on connection", nullptr, ERR_CONNECT);
+    if (status == -1) handleError("error on connection", NULL, ERR_CONNECT);
     
-    // inform server that interfaceMonitor is ready
-    status = write(sockFd, "READY", 6);
-    if (status == -1)
-        handleError("error on connection", nullptr, ERR_CONNECT);
+    // inform server that interfaceMonitor is ready and read the instruction
+    talk("Ready", box);
+    if (strcmp(box, "Monitor") != 0)
+        handleError("didn't undrestand the instruction", NULL, ERR_CONNECT);
+    else talk("Monitoring", NULL);
     
-    // read the instructed interfaces to monitor
-    bzero(temp, MAX_BUF);
-    status = read(sockFd, temp, MAX_BUF);
-    if (status == -1)
-        handleError("error on connection", nullptr, ERR_CONNECT);
-    std::vector<std::string> instructedInterfaces;
-    char* word;
-    while( (word = strtok(temp, " ")) ) instructedInterfaces.push_back(std::string(word));
-    
-    // opend the /sys/class/net directory to monitor stats
-    DIR* dp = opendir(netPath);
-    if (dp == NULL)
-        handleError("could not open interfaces directory.", nullptr, ERR_OPEN_DIR);
-    struct dirent* dirp;
-    std::vector<int> childPids;
-    pid_t pid = getpid();
-    int statistics[NUM_STAT];
-    while( (dirp = readdir(dp)) ) { /// find interfaces in directory and spwan child to monitor
-        bool found = false;
-        for (int i = 0; i < instructedInterfaces.size() && !found; i++) {
-            if ( instructedInterfaces[i] == std::string(dirp->d_name) ) found = true;
-        }
-        if (found) { /// this interface was requested to be monitored
-            pid = fork();
-            if (pid == -1)
-                handleError("could not open interfaces directory.", nullptr, ERR_FORK);
-            else if (pid == 0) { /// child needs to keep monitor until gets intrupted by signal
-                while (isRunning) {
-                    loadStatistics(dirp->d_name, statistics);
-                    // print
-                    // check if interface down -> report -> set up if requested
+    // open the /sys/class/net/<interface> directory and monitor stats
+    std::ifstream ifs;
+    struct ifreq ifr;
+    std::cout << "Interface: " << argv[1] << " ";
+    while (isRunning) {
+        for (int i = 0; i < NUM_STAT; i++) {
+            bzero(path, MIN_BUF);
+            strcpy(path, netPath);
+            strcpy(path, STAT[i].subPath);
+            if (i > 2) strcat(path,STAT[i].title);
+            ifs.open(path);
+            if(ifs.is_open()) {
+                if (i == 0) {
+                    bzero(box, MIN_BUF);
+                    ifs >> box;
+                    if (strcmp(box, "up") == 0) STAT[i].val = UP;
+                    else if (strcmp(box, "down") == 0) STAT[i].val = DOWN;
+                    else STAT[i].val = UNKNOWN;
                 }
+                else ifs >> STAT[i].val;
+                ifs.close();
+                std::cout << STAT[i].title << ": " << STAT[i].val << " ";
+                if (i == 2 || i == NUM_STAT) std::cout << std::endl;
             }
-            else childPids.push_back(pid);
+            else handleError("\ncouldn't open a statistic file", NULL, ERR_OPEN_DIR);
         }
-    }
-    
-    // here parnt checks if any child goes down, it restart the process again
-    while (isRunning && pid > 0) {
-        pid = wait(&status);
-        bool found = false;
-        for (int i = 0; i < childPids.size() && !found; i++) {
-            if (pid == childPids[i]) found = true;
-            childPids[i] = fork();
-            if (childPids[i] == 0) {
-                //  MONITOR
+        std::cout << std::endl;
+        
+        // check if interface down -> report -> set up if requested
+        if (STAT[0].val == DOWN) {
+            talk("Link Down", box);
+            if (strcmp(box, "Set Link Up") == 0) {
+                bzero(&ifr, sizeof(ifr));
+                strncpy(ifr.ifr_name, argv[1], IFNAMSIZ);
+                ifr.ifr_ifru.ifru_flags |= IFF_UP;
+                status = ioctl(sockFd, SIOCSIFFLAGS, &ifr);
+                if (status == -1) strerror(errno);
             }
         }
+        
+        sleep(1);
     }
+
     
     return 0;
 }
 
 
-void handleError(std::string msg1, char* msg2, exit_err_code err, int fd) {
-    std::cout << msg1;
-    if(msg2) std::cout << msg2;
-    std::cout << std::endl;
-    if (fd > 0) close(fd);
-    if (err != IGNORE) {
-        strerror(errno);
-        exit(err);
-    }
-}
-
-void loadStatistics(char* interfaceName, int* stats) {
-    bzero(path, MAX_BUF);
-    strcpy(path, statPath);
-    sprintf(path, path, interfaceName);
-    std::ifstream ifs;
-    for (int i = 0; i < NUM_STAT; i++) {
-        bzero(temp, MAX_BUF);
-        strcpy(temp, path);
-        strcat(temp,statsTitle[i]);
-        ifs.open(temp);
-        if(ifs.is_open()) {
-            ifs >> stats[i];
-            ifs.close();
-        }
-        else handleError("couldn't open a statistic for ", interfaceName);
-    }
-}
-
-static void sigHandler(int signal) {
+static void signalHandler(int signal) {
     switch(signal) {
         case SIGINT:
             isRunning = false;
@@ -164,3 +152,24 @@ static void sigHandler(int signal) {
     }
 }
 
+void handleError(std::string msg1, char* msg2, exit_err_code err, int fd) {
+    std::cout << msg1;
+    if(msg2) std::cout << msg2;
+    std::cout << std::endl;
+    if (fd > 0) close(fd);
+    if (err != ALERT) {
+        strerror(errno);
+        exit(err);
+    }
+}
+
+void talk(const char* toSay, char* answer) {
+    int status = write(sockFd, toSay, strlen(toSay)+1);
+    if (status == -1) handleError("error on connection", NULL, ERR_CONNECT);
+    
+    if (answer) { /// !NULL
+        bzero(answer, MIN_BUF);
+        status = read(sockFd, box, MIN_BUF);
+        if (status == -1) handleError("error on connection", NULL, ERR_CONNECT);
+    }
+}
